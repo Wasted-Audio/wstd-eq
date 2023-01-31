ee853dc868e874a47a73b9b44447859e9272a87b
dreamer <alexander@puikheid.nl>
2023-01-31 22:06:48 +0100

    cleanup; set override files

---
+++ override/DistrhoPluginInfo.h
@@ -0,0 +1,30 @@
+/** Copyright (c) Wasted Audio 2023 - GPL-3.0-or-later */
+
+#pragma once
+
+#define DISTRHO_PLUGIN_NAME                 "WSTD EQ"
+#define DISTRHO_PLUGIN_URI                  "http://wasted.audio/lv2/plugin/WSTD_EQ"
+#define DISTRHO_PLUGIN_CLAP_ID              "http://wasted.audio/lv2/plugin/WSTD_EQ"
+#define DISTRHO_PLUGIN_NUM_INPUTS           2
+#define DISTRHO_PLUGIN_NUM_OUTPUTS          2
+#define DISTRHO_PLUGIN_IS_SYNTH             0
+#define DISTRHO_PLUGIN_HAS_UI               1
+#define DISTRHO_PLUGIN_IS_RT_SAFE           1
+#define DISTRHO_PLUGIN_WANT_PROGRAMS        0
+#define DISTRHO_PLUGIN_WANT_STATE           0
+#define DISTRHO_PLUGIN_WANT_TIMEPOS         1
+#define DISTRHO_PLUGIN_WANT_FULL_STATE      0
+#define DISTRHO_PLUGIN_WANT_MIDI_INPUT      0
+#define DISTRHO_PLUGIN_WANT_MIDI_OUTPUT     0
+#define DISTRHO_PLUGIN_LV2_CATEGORY         "lv2:EQPlugin"
+#define DISTRHO_PLUGIN_VST3_CATEGORIES      "Fx|EQ|Stereo"
+#define DISTRHO_PLUGIN_CLAP_FEATURES        "audio-effect", "equalizer", "stereo"
+
+// for level monitoring
+#define DISTRHO_PLUGIN_WANT_DIRECT_ACCESS   0
+
+#define DISTRHO_UI_USE_CUSTOM               1
+#define DISTRHO_UI_CUSTOM_INCLUDE_PATH      "DearImGui.hpp"
+#define DISTRHO_UI_CUSTOM_WIDGET_TYPE       DGL_NAMESPACE::ImGuiTopLevelWidget
+#define DISTRHO_UI_DEFAULT_WIDTH            120
+#define DISTRHO_UI_DEFAULT_HEIGHT           500
---
+++ override/HeavyDPF_WSTD_EQ_UI.cpp
@@ -0,0 +1,201 @@
+/*
+ * ImGui plugin example
+ * Copyright (C) 2021 Jean Pierre Cimalando <jp-dev@inbox.ru>
+ * Copyright (C) 2021-2022 Filipe Coelho <falktx@falktx.com>
+ * SPDX-License-Identifier: ISC
+ */
+
+#include "DistrhoUI.hpp"
+#include "ResizeHandle.hpp"
+
+START_NAMESPACE_DISTRHO
+
+// --------------------------------------------------------------------------------------------------------------------
+
+class ImGuiPluginUI : public UI
+{
+    float ffreq = 1337.0f;
+    float fhigh = 0.0f;
+    float flow = 0.0f;
+    float fmid = 0.0f;
+    
+    ResizeHandle fResizeHandle;
+
+    // ----------------------------------------------------------------------------------------------------------------
+
+public:
+   /**
+      UI class constructor.
+      The UI should be initialized to a default state that matches the plugin side.
+    */
+    ImGuiPluginUI()
+        : UI(DISTRHO_UI_DEFAULT_WIDTH, DISTRHO_UI_DEFAULT_HEIGHT),
+          fResizeHandle(this)
+    {
+        setGeometryConstraints(DISTRHO_UI_DEFAULT_WIDTH, DISTRHO_UI_DEFAULT_HEIGHT, true);
+
+        // hide handle if UI is resizable
+        // if (isResizable())
+        fResizeHandle.hide();
+    }
+
+protected:
+    // ----------------------------------------------------------------------------------------------------------------
+    // DSP/Plugin Callbacks
+
+   /**
+      A parameter has changed on the plugin side.@n
+      This is called by the host to inform the UI about parameter changes.
+    */
+    void parameterChanged(uint32_t index, float value) override
+    {
+        switch (index) {
+            case 0: {
+                ffreq = value;
+                break;
+            }
+            case 1: {
+                fhigh = value;
+                break;
+            }
+            case 2: {
+                flow = value;
+                break;
+            }
+            case 3: {
+                fmid = value;
+                break;
+            }
+            
+            default: return;
+        }
+    
+        repaint();
+    }
+
+    // ----------------------------------------------------------------------------------------------------------------
+    // Widget Callbacks
+
+   /**
+      ImGui specific onDisplay function.
+    */
+    void onImGuiDisplay() override  
+    {
+        
+        const float width = getWidth();
+        const float height = getHeight();
+        // const float margin = 20.0f * getScaleFactor();
+        const float margin = 0.0f;
+
+        ImGui::SetNextWindowPos(ImVec2(margin, margin));
+        ImGui::SetNextWindowSize(ImVec2(width - 2 * margin, height - 2 * margin));
+        
+        ImGuiStyle& style = ImGui::GetStyle();
+        style.WindowTitleAlign = ImVec2(0.5f, 0.5f);
+
+        style.Colors[ImGuiCol_TitleBgActive] = (ImVec4)ImColor::HSV(3.31f / 3.6f, 0.64f, 0.40f);
+        style.Colors[ImGuiCol_WindowBg] = (ImVec4)ImColor::HSV(3.31f / 3.6f, 0.64f, 0.10f);
+
+        ImFont* defaultFont = ImGui::GetFont();
+        ImFont* titleBarFont = new ImFont(*defaultFont);
+        titleBarFont->Scale = 1.3f;
+
+        ImGui::PushFont(titleBarFont);
+        if (ImGui::Begin("WSTD EQ", nullptr, ImGuiWindowFlags_NoResize + ImGuiWindowFlags_NoCollapse))
+        {
+
+            ImGui::Dummy(ImVec2(0.0f, 8.0f));
+            ImGui::PushFont(defaultFont);
+            auto ImGuiKnob_Flags = ImGuiKnobFlags_DoubleClickReset + ImGuiKnobFlags_ValueTooltip + ImGuiKnobFlags_NoInput + ImGuiKnobFlags_ValueTooltipHideOnClick;
+            auto ImGuiKnob_FlagsDB = ImGuiKnob_Flags + ImGuiKnobFlags_dB;
+            auto ImGuiKnob_FlagsLog = ImGuiKnob_Flags + ImGuiKnobFlags_Logarithmic;
+
+            ImGui::PushStyleColor(ImGuiCol_ButtonActive,    (ImVec4)ImColor::HSV(2.04f / 3.6f, 0.83f, 0.64f));
+            ImGui::PushStyleColor(ImGuiCol_ButtonHovered,   (ImVec4)ImColor::HSV(2.04f / 3.6f, 0.83f, 0.84f));
+            if (ImGuiKnobs::Knob("High", &fhigh, -15.0f, 15.0, 0.2f, "%.1fdB", ImGuiKnobVariant_SteppedTick, 100, ImGuiKnob_FlagsDB, 7))
+            {
+
+                if (ImGui::IsItemActivated())
+                {
+                    editParameter(1, true);
+                    if (ImGui::IsMouseDoubleClicked(0))
+                        fhigh = 0.0f;
+                }
+                setParameterValue(1, fhigh);
+            }
+            ImGui::PopStyleColor();
+            ImGui::PopStyleColor();
+
+            ImGui::PushStyleColor(ImGuiCol_ButtonActive,    (ImVec4)ImColor::HSV(1.6f / 3.6f, 0.77f, 0.64f));
+            ImGui::PushStyleColor(ImGuiCol_ButtonHovered,   (ImVec4)ImColor::HSV(1.6f / 3.6f, 0.77f, 0.74f));
+            if (ImGuiKnobs::Knob("Mid", &fmid, -15.0f, 15.0, 0.2f, "%.1fdB", ImGuiKnobVariant_SteppedTick, 100, ImGuiKnob_FlagsDB, 7))
+            {
+                if (ImGui::IsItemActivated())
+                {
+                    editParameter(3, true);
+                    if (ImGui::IsMouseDoubleClicked(0))
+                        fmid = 0.0f;
+                }
+                setParameterValue(3, fmid);
+            }
+            ImGui::PopStyleColor();
+            ImGui::PopStyleColor();
+    
+            ImGui::SetCursorPosX(23.0f);
+            ImGui::PushStyleColor(ImGuiCol_ButtonActive,    (ImVec4)ImColor::HSV(1.6f / 3.6f, 0.77f, 0.44f));
+            ImGui::PushStyleColor(ImGuiCol_ButtonHovered,   (ImVec4)ImColor::HSV(1.6f / 3.6f, 0.77f, 0.64f));
+            if (ImGuiKnobs::Knob("Mid Freq", &ffreq, 313.3f, 5705.6f, 50.0f, "%.1fHz", ImGuiKnobVariant_SteppedTick, 70, ImGuiKnob_FlagsLog, 11))
+            {
+                if (ImGui::IsItemActivated())
+                {
+                    editParameter(0, true);
+                    if (ImGui::IsMouseDoubleClicked(0))
+                        ffreq = 1337.0f;
+                }
+                setParameterValue(0, ffreq);
+            }
+            ImGui::PopStyleColor();
+            ImGui::PopStyleColor();
+    
+            ImGui::PushStyleColor(ImGuiCol_ButtonActive,    (ImVec4)ImColor::HSV(0.03f / 3.6f, 0.76f, 0.74f));
+            ImGui::PushStyleColor(ImGuiCol_ButtonHovered,   (ImVec4)ImColor::HSV(0.03f / 3.6f, 0.76f, 0.84f));
+            if (ImGuiKnobs::Knob("Low", &flow, -15.0f, 15.0, 0.2f, "%.1fdB", ImGuiKnobVariant_SteppedTick, 100, ImGuiKnob_FlagsDB, 7))
+            {
+                if (ImGui::IsItemActivated())
+                {
+                    editParameter(2, true);
+                    if (ImGui::IsMouseDoubleClicked(0))
+                        flow = 0.0f;
+                }
+                setParameterValue(2, flow);
+            }
+            ImGui::PopStyleColor();
+            ImGui::PopStyleColor();
+    
+            if (ImGui::IsItemDeactivated())
+            {
+                editParameter(0, false);
+                editParameter(1, false);
+                editParameter(2, false);
+                editParameter(3, false);
+            }
+
+            ImGui::PopFont();
+        }
+        ImGui::PopFont();
+        ImGui::End();
+    }
+
+    DISTRHO_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ImGuiPluginUI)
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UI* createUI()
+{
+    return new ImGuiPluginUI();
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+END_NAMESPACE_DISTRHO
